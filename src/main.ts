e0345fe4f76ed1258f7816f6a2ca859865e35069
luqj <ken1433883954@163.com>
2026-06-02 17:58:55 +0800

    静态资源引入

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from "pinia";
 import { getCategoryAPI } from "@/apis/test";
 import App from "./App.vue";
 import router from "./router";
+import "@/styles/common.scss";
 
 getCategoryAPI().then((res) => {
   console.log(res);
