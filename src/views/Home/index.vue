e2682568af8986d9be5747e024d76aafba797326
luqj <ken1433883954@163.com>
2026-06-04 14:28:00 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,6 +6,13 @@
   <HomeNew />
   <HomeHot />
   <HomeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanal title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>我是新鲜好物</div>
+  </HomePanal>
+  <HomePanal title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>我是人气推荐</div>
+  </HomePanal>
 </template>
 
 <script setup lang="ts">
@@ -14,4 +21,5 @@ import HomeNew from "./components/HomeNew.vue";
 import HomeBanner from "./components/HomeBanner.vue";
 import HomeCategory from "./components/HomeCategory.vue";
 import HomeHot from "./components/HomeHot.vue";
+import HomePanal from "./components/HomePanal.vue";
 </script>
