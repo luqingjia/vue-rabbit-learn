92edde4d2208d5dbfea7ae5a0502dae37c041023
luqj <ken1433883954@163.com>
2026-06-04 14:54:44 +0800

    新鲜好物构建

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container">
     <HomeCategory />
+
     <HomeBanner />
   </div>
+
   <HomeNew />
+
   <HomeHot />
+
   <HomeProduct />
+
   <!-- 测试面板组件 -->
   <HomePanal title="新鲜好物" sub-title="新鲜好物 好多商品">
     <div>我是新鲜好物</div>
   </HomePanal>
+
   <HomePanal title="人气推荐" sub-title="人气推荐 好多商品">
     <div>我是人气推荐</div>
   </HomePanal>
