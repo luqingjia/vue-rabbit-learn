3de49677c399e1548d326d0a0c151bdfc2dccacc
luqj <ken1433883954@163.com>
2026-06-04 15:05:33 +0800

    推荐好物构建

--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -1,1 +1,76 @@
-<template>我是热门</template>
+<script setup lang="ts">
+import HomePanal from "./HomePanal.vue";
+import { findHotAPI } from "@/apis/home";
+import { ref, onMounted } from "vue";
+
+const hotList = ref([]);
+
+onMounted(async () => {
+  const res = await findHotAPI();
+  hotList.value = res.result;
+});
+</script>
+
+<template>
+  <HomePanal title="人气推荐" sub-title="人气推荐 好多商品">
+    <ul class="goods-list">
+      <li v-for="item in hotList" :key="item.id">
+        <RouterLink to="/">
+          <img :src="item.picture" alt="" />
+          <p class="name">{{ item.name }}</p>
+          <p class="price">&yen;{{ item.price }}</p>
+        </RouterLink>
+      </li>
+    </ul>
+  </HomePanal>
+  <!-- 下面是插槽主体内容模版
+  <ul class="goods-list">
+    <li v-for="item in newList" :key="item.id">
+      <RouterLink to="/">
+        <img :src="item.picture" alt="" />
+        <p class="name">{{ item.name }}</p>
+        <p class="price">&yen;{{ item.price }}</p>
+      </RouterLink>
+    </li>
+  </ul>
+  -->
+</template>
+
+<style scoped lang="scss">
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 406px;
+
+  li {
+    width: 306px;
+    height: 406px;
+
+    background: #f0f9f4;
+    transition: all 0.5s;
+
+    &:hover {
+      transform: translate3d(0, -3px, 0);
+      box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+    }
+
+    img {
+      width: 306px;
+      height: 306px;
+    }
+
+    p {
+      font-size: 22px;
+      padding-top: 12px;
+      text-align: center;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .price {
+      color: $priceColor;
+    }
+  }
+}
+</style>
