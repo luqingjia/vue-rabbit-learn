87617af2d95a8780daa6455a6b26b42f63673601
luqj <ken1433883954@163.com>
2026-06-05 10:04:37 +0800

    二级分类

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import Layout from "@/views/Layout/index.vue";
 import Login from "@/views/Login/index.vue";
 import Home from "@/views/Home/index.vue";
 import Categroy from "@/views/Categroy/index.vue";
+import SubCategory from "@/views/SubCategory/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,10 @@ const router = createRouter({
         {
           path: "category/:id",
           component: Categroy,
+        },
+        {
+          path: "category/sub/:id",
+          component: SubCategory,
         },
       ],
     },
